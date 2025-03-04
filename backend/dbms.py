eb3ecd2932e050e36a54e8c83de3ed8fabd26810
Hunter Lindauer <holindauer@gmail.com>
2025-03-04 13:36:26 -0800

    impl: admin account creation backend

--- backend/dbms.py
+++ backend/dbms.py
@@ -19,19 +19,13 @@ class Database:
         self.cursor = self.connection.cursor()
         print("Cursor created...")
 
-        # contains salts used for hashing user passwords
-        # salts are constant across passwords
-        # self.username_salts = {}
-
-
     def __del__(self):
         """On class destruction, close connection to db"""
         self.connection.close()
         print("Connection to database terminated...")
 
-
     def customer_account_creation(self, username: str, password: str, email: str, phone_number: str) -> bool:
-
+        """ Insert customer data into db if no previous account exists"""
         # don't create account if already exists
         if self._does_customer_exist(username):
             return False
@@ -43,6 +37,23 @@ class Database:
         self.cursor.execute(            
             "INSERT INTO CustomerUser (Customer_ID, Username, Email, Phone_Number, Password, Salt) VALUES (?, ?, ?, ?, ?, ?)", 
             (customer_id, username, email, phone_number, pw_hash, salt)
+            )
+        self.connection.commit()
+        return True
+
+    def admin_account_creation(self, username: str, password: str) -> bool:
+        """ Insert admin data into db if no previous account exists"""
+        # don't create account if already exists
+        if self._does_admin_exist(username):
+            return False
+        # hash salted password
+        pw_hash, salt = self._hash_password(password)
+        # generate customer id artificial key
+        admin_id = self._new_admin_id()
+        # insert customer data into db
+        self.cursor.execute(            
+            "INSERT INTO AdminUser (Admin_ID, Username, Password, Salt) VALUES (?, ?, ?, ?)", 
+            (admin_id, username, pw_hash, salt)
             )
         self.connection.commit()
         return True
@@ -76,6 +87,13 @@ class Database:
             return 0
         return max_customer_id + 1
 
+    def _new_admin_id(self) -> int:
+        """generate new admin id artifical key"""
+        self.cursor.execute("SELECT MAX(Admin_ID) FROM AdminUser")
+        max_admin_id = self.cursor.fetchone()[0]
+        if max_admin_id is None:
+            return 0
+        return max_admin_id + 1
 
 
 
--- backend/test_dbms.py
+++ backend/test_dbms.py
@@ -5,6 +5,10 @@ def test_first_customer_id():
     db = Database("database.db")
     assert db._new_customer_id() == 0
 
+def test_first_admin_id():
+    db = Database("database.db")
+    assert db._new_admin_id() == 0
+
 def test_no_customer_found():
     db = Database("database.db")
     assert db._does_customer_exist("test") == False
@@ -13,10 +17,16 @@ def test_no_admin_found():
     db = Database("database.db")
     assert db._does_admin_exist("test") == False
 
-
 def test_customer_account_creation():
     db = Database("database.db")
     assert db._does_customer_exist("test") == False
     success = db.customer_account_creation("test", "test", "test@test.com", "1111111111")
     assert success == True
     assert db._does_customer_exist("test") == True
+
+def test_admin_account_creation():
+    db = Database("database.db")
+    assert db._does_admin_exist("test") == False
+    success = db.admin_account_creation("test", "test")
+    assert success == True
+    assert db._does_admin_exist("test") == True
