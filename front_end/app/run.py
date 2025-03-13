04b3cc786ecdce8dde96fc79963d6a825963374d
ChandlerGu3 <Chandler.Gu3@gmail.com>
2025-03-12 21:19:06 -0700

    feat: admin Personal Information

--- front_end/app/run.py
+++ front_end/app/run.py
@@ -77,7 +77,7 @@ def user_profile(username):
     return redirect(url_for('user_profile_page', username=username, page='PersonalInformation'))
 
 @app.route('/Profile/<username>/<page>')
-def user_profile_page(username, page="PersonalInformation"):
+def user_profile_page(username, page):
     # if user is not logged  in redirect page not found
     if g.user is None:
         abort(404)
@@ -86,8 +86,24 @@ def user_profile_page(username, page="PersonalInformation"):
     if username != g.user.username:
         redirect(url_for('user_profile_page', username=g.user.username, page=page))
     
-    try:
-        return render_template(f'Profile/{g.user.userType}/{page}.html')
+    try:        
+        if g.user.userType == 'Admin':
+            if page == 'PersonalInformation':
+                pageData = {'username': g.user.username}
+            elif page == 'Products':
+                pageData = {}
+            elif page == 'Analytics':
+                pageData = {}
+            
+        elif g.user.userType == 'Customer':    
+            if page == 'PersonalInformation':
+                pageData = {'email': g.user.username, 'PhoneNumber': '555-555-5555'}
+            elif page == 'Orders':
+                pageData = {}
+            elif page == 'Wishlist':
+                pageData = {}
+                
+        return render_template(f'Profile/{g.user.userType}/{page}.html', pageData=pageData)
     except:
         abort(404)
 
